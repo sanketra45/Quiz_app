2af84a4bee172a40d8e9f4ecc52535061aa05f40
Sanket Rathod <sanketra53@gmail.com>
2025-10-06 00:51:48 +0530

    Done3

--- Quiz-app/src/App.jsx
+++ Quiz-app/src/App.jsx
@@ -4,7 +4,6 @@ import Quiz from './Components/Quiz/Quiz.jsx'
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
       <Quiz />
     </div>
   )
--- Quiz-app/src/Components/Quiz/Quiz.jsx
+++ Quiz-app/src/Components/Quiz/Quiz.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Quiz.css'
+import { data } from '../../data';
 
 const Quiz = () => {
+
+  const [Index, setIndex] = useState(0); 
+  const [Question, setQuestion] = useState(data[Index]);
+
+  const checkAns = (e, ans)=>{
+    if(Question.ans === ans){
+      e.target.classlist.add("Correct");
+    }
+    else{
+      e.target.classlist.add("Wrong");
+    }
+  }
+
   return (
+
     <div className='container'>
       <h1>Quiz App</h1>
       <hr />
-      <h2>Which of the following is a frontend JavaScript framework?</h2>
+      <h2>{Index+1}.{Question.question}</h2>
       <ul>
-        <li>Django</li>
-        <li>React</li>
-        <li>Flask</li>
-        <li>Spring</li>
+        <li onClick={(e)=>{checkAns(e, 1)}}>{Question.option1}</li>
+        <li onClick={(e)=>{checkAns(e, 2)}}>{Question.option2}</li>
+        <li onClick={(e)=>{checkAns(e, 3)}}>{Question.option3}</li>
+        <li onClick={(e)=>{checkAns(e, 4)}}>{Question.option4}</li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 Questions</div>
